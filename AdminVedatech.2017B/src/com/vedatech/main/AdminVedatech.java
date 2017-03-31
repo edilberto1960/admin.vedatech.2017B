fa8d97c5cf28d4e9ad7bc986d6f2af960a65dd3a
edilberto1960 <e.ramos.salinas@gmail.com>
2017-03-31 09:54:05 -0600

    Change-Id: I0000000000000000000000000000000000000002 Signed-off-by: edilberto1960 <e.ramos.salinas@gmail.com>

--- AdminVedatech.2017B/src/com/vedatech/main/AdminVedatech.java
+++ AdminVedatech.2017B/src/com/vedatech/main/AdminVedatech.java
@@ -5,6 +5,7 @@ public class AdminVedatech {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer codigo");
+		System.out.println("Segundo codigo");
 
 	}
 
